8a795a0aadba12b9ecca593aebff0370e33b5604
Varadharajan <Varadharajan@Varadharajan-PC>
2014-06-05 08:30:21 +0530

    Latest commit by varadharajan.R

--- grubpost/com/vail/foodcourt/controller/RedirctionController.java
+++ grubpost/com/vail/foodcourt/controller/RedirctionController.java
@@ -1,13 +1,17 @@
 package com.vail.foodcourt.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RedirctionController {
+	@Value("$(local_oauth_client_id)")
+	private String msg;
     @RequestMapping(value = "/")
     public String adminPage() {
         System.out.println("-()-()-()-()-()-()-()-(\"STARTUP\")-()-()-()-()-()-()-()");
+       System.out.println(msg);
         return "uploadExcel";
     }
 
