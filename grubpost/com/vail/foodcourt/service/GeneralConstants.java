f20319c6796c1ade2ad32b04eb004c10d301cb7b
Varadharajan <Varadharajan@Varadharajan-PC>
2014-06-07 08:57:20 +0530

    Added method and class for while project Initialization on Server.
    Signed-off-by: Varadharajan <Varadharajan@Varadharajan-PC>

--- grubpost/com/vail/foodcourt/service/GeneralConstants.java
+++ grubpost/com/vail/foodcourt/service/GeneralConstants.java
@@ -5,4 +5,60 @@ public class GeneralConstants {
     
     public static final String ALPHA_NUMERIC_STRING = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
 
+    
+
+
+        /** The Constant EMPTY_STRING. */
+        public static final String EMPTY_STRING = "";
+
+        /** The Constant SINGLE_SPACE. */
+        public static final String SINGLE_SPACE = " ";
+
+        /** The Constant EQUALS. */
+        public static final String EQUALS = "=";
+        
+        /** The Constant DOT. */
+        public static final String DOT = ".";
+        
+        /** The Constant COMMA. */
+        public static final String COMMA = ",";
+        
+        /** The Constant COLON. */
+        public static final String COLON = ":";
+
+        /** The Constant SEMI_COLON. */
+        public static final String SEMI_COLON = ";";
+
+        /** The Constant FRONT_SLASH. */
+        public static final String FRONT_SLASH = "/";
+        
+        /** The Constant UNDER_SCORE. */
+        public static final String UNDER_SCORE = "_";
+
+        /** The Constant NEWLINE. */
+        public static final String NEWLINE = "\n";
+
+        /** The Constant AT_THE_RATE. */
+        public static final String AT_THE_RATE = "@";
+
+        /** The Constant FTP_PROTOCOL. */
+        public static final String FTP_PROTOCOL = "ftp://";
+
+        /** The Constant SFTP_PROTOCOL. */
+        public static final String SFTP_PROTOCOL = "sftp://";
+
+        /** The Constant FTP. */
+        public static final String FTP = "FTP";
+
+        /** The Constant SFTP. */
+        public static final String SFTP = "SFTP";
+
+        /** The Constant APPLICATION_STARTUP_STATUS. */
+        public static final String APPLICATION_STARTUP_STATUS = "STARTUP_STATUS";
+
+        /** The Constant EXECUTION_LOGGER. */
+        public static final String EXECUTION_LOGGER = "EXECUTION_LOGGER";
+    
+
+    
 }
--- grubpost/com/vail/foodcourt/service/ResumeProject.java
+++ grubpost/com/vail/foodcourt/service/ResumeProject.java
@@ -1,5 +1,6 @@
 package com.vail.foodcourt.service;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,9 +16,11 @@ public class ResumeProject extends HttpServlet {
      */
 
 	private static final long serialVersionUID = 1L;
-
+	Properties folderCreationproperties;
 	public void init(ServletConfig config) {
 		readPropertiesFile("resumeApplication.properties");
+		folderProducePool();
+		resumeLogger();
 	}
 
 	public void readPropertiesFile(String propertyFileName) {
@@ -28,14 +31,32 @@ public class ResumeProject extends HttpServlet {
 				throw new FileNotFoundException("property file '"
 						+ propertyFileName + "' not found in the classpath");
 			}
-			Properties properties = new Properties();
-			properties.load(inputStream);
-			String url = properties.getProperty("oauthscopes");
-			System.out.println(url);
+			folderCreationproperties = new Properties();
+			folderCreationproperties.load(inputStream);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 	}
+	public void folderProducePool(){
+		folderProduce("resumeProjectLog");
+		folderProduce("coreLog");
+		folderProduce("dbLog");
+		folderProduce("dataRetriveLog");
+		folderProduce("xml");
+		
+	}
+	public void folderProduce(String folderName){
+		File folder=new File(folderCreationproperties.getProperty("foodCourtApp")+GeneralConstants.FRONT_SLASH+folderName);
+		if(!folder.exists())
+		folder.mkdir();
+	}
+	public void resumeLogger(){
+		Properties logFilesProp=new Properties();
+		logFilesProp.put("log4j.appender.APPL_LOG.File", "");
+		
+	}
+	
+	
 
 }
